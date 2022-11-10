466c1b41e2dd7092904cae9bb7bcfe62673356d3
bert <49372063+bertman80@users.noreply.github.com>
2022-11-10 18:12:55 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,17 @@
 #If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
      try {
-         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+         Invoke-RestMethod https://github.com/bertman80/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
          Write-Host "The profile @ [$PROFILE] has been created."
      }
      catch {
          throw $_.Exception.Message
      }
- }
-# If the file already exists, show the message and do nothing.
- else {
-		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
-		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
- }
+ } else {
+	Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
+	Invoke-RestMethod https://github.com/bertman80/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+	Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+}
 & $profile
 
 # OMP Install
